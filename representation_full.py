ba8f5414ea108fc4732697fb80f2ac589de954ae
felix✌🏻 <162877035+schwienernitzel@users.noreply.github.com>
2024-10-23 15:22:24 +0200

    XMLparser: `representation_full.py:` Remove global title definition(s)
    Signed-off-by: felix✌🏻 <162877035+schwienernitzel@users.noreply.github.com>
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- representation_full.py
+++ representation_full.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 
-data = pd.read_csv('out/output_annotated.csv', delimiter='\t', header=None)
+data = pd.read_csv('out/output_migration-annotated.csv', delimiter='\t', header=None)
 
 tags = data[3]
 tag_counts = Counter(tags)
@@ -16,6 +16,5 @@ for label, size in zip(labels, sizes):
 
 plt.figure(figsize=(8, 6))
 plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-plt.title('Verteilung gemessen an der Gesamtmenge')
 plt.axis('equal')
 plt.show()
